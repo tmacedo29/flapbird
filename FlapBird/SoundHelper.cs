98a9850194c6a49c3e712204c4e9e7cc9adf925a
Thiago Macedo <tmacedo29@gmail.com>
2024-10-29 08:37:15 -0300

    Adicionado loop

--- FlapBird/SoundHelper.cs
+++ FlapBird/SoundHelper.cs
@@ -7,12 +7,13 @@ public class SoundHelper
 {
   //------------------------------------------------------------------------
 
-  public static void Play(string nomeArquivoWav)
+  public static void Play(string nomeArquivoWav, bool loop = false)
   {
     Task.Run(async () =>
     {
       var audioFX = await FileSystem.OpenAppPackageFileAsync(nomeArquivoWav);
       var audioPlayer = AudioManager.Current.CreatePlayer(audioFX);
+      audioPlayer.Loop = loop;
       audioPlayer.Play();
     });
   }
